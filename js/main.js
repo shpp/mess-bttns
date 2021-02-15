53409f99ee958c0ce21b628331e0e5bd05314993
Andrii Sadov <sadovam@gmail.com>
2021-02-15 21:02:50 +0200

    Update config

--- js/main.js
+++ js/main.js
@@ -1,56 +1,57 @@
 document.body.onload = function() {
     console.log("hello");
     
-    const messangers = {
-      email: {
-        icon: '<i class="fas fa-envelope-square"></i>',
-        pre: "mailto:"
-      },
-      phone: {
-        icon: '<i class="fas fa-phone-square"></i>',
-        pre: "tel:"
-      },
-      viber: {
-        icon: '<i class="fab fa-viber"></i>',
-        pre: "viber://chat?number=%2B"
-      },
-      facebook: {
-        icon: '<i class="fab fa-facebook-messenger"></i>',
-        pre: "fb-messenger://user-thread/"
-      },
-      telegram: {
-        icon: '<i class="fab fa-telegram"></i>',
-        pre: "https://t.me/"
-      }
-    };
     
     const config = {
-      color: "green",
-      messangers: [
-        {name: "viber", contact: "380681317564"},
-        {name: "phone", contact: "+380681317564"},
-        {name: "email", contact: "sadovam@gmail.com"},
-        {name: "telegram", contact: "sadovam"},
-        {name: "facebook", contact: "100007885214113"},
-      ]
-
+      color: "#27ae60",
+      messengers: {
+        viber: "380681317564",
+        phone: "+380681317564",
+        email: "sadovam@gmail.com",
+        telegram: "sadovam",
+        facebook: "100007885214113",
+      }
     };
-    makeMess(config, messangers);
+    makeMess(config);
 };
 
 
 
-function makeMess(config, messangers) {
+function makeMess(config) {
   
+  const messengers = {
+    email: {
+      icon: '<i class="fas fa-envelope-square"></i>',
+      pre: "mailto:"
+    },
+    phone: {
+      icon: '<i class="fas fa-phone-square"></i>',
+      pre: "tel:"
+    },
+    viber: {
+      icon: '<i class="fab fa-viber"></i>',
+      pre: "viber://chat?number=%2B"
+    },
+    facebook: {
+      icon: '<i class="fab fa-facebook-messenger"></i>',
+      pre: "fb-messenger://user-thread/"
+    },
+    telegram: {
+      icon: '<i class="fab fa-telegram"></i>',
+      pre: "https://t.me/"
+    }
+  };
+  
+
   const btnsBlock = document.createElement("div");
   btnsBlock.className = "mess__btns-block mess__btns-block--hidden";
   document.body.appendChild(btnsBlock);
   
-  for (m of config.messangers) {
+  for (let m in config.messengers) {
     let elm = document.createElement("a");
-    elm.href = messangers[m.name].pre + m.contact;
+    elm.href = messengers[m].pre + config.messengers[m];
     elm.className = "mess__link";
-    elm.innerHTML = messangers[m.name].icon;
+    elm.innerHTML = messengers[m].icon;
     elm.target = "_blank";
     elm.style.color = config.color;
     btnsBlock.appendChild(elm);
